4f4bd820e5ee056a98fb952c3dad4eb3dda96ef8
anantariskys <gamersindo555@gmail.com>
2024-12-17 20:17:52 +0700

    Create sosialMedia.js

---
+++ src/data/sosialMedia.js
@@ -0,0 +1,21 @@
+export  const  sosialMedia = [
+    {
+        id: 1,
+        name: "Instagram",
+        url: "https://www.instagram.com/riskykun_/",
+        icon: "mdi:instagram",
+    },
+    {
+        id: 2,
+        name: "Github",
+        url: "https://github.com/anantariskys",
+        icon: "mdi:github",
+    },
+    {
+        id: 3,
+        name: "Linkedin",
+        url: "www.linkedin.com/in/anantariskys",
+        icon: "mdi:linkedin",
+    }
+]
+   
