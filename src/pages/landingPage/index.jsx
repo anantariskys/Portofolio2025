3372dbedf280ec1d427cb6b6e35a9fc15bcfbefa
anantariskys <gamersindo555@gmail.com>
2024-12-20 20:24:32 +0700

    Update index.jsx

--- src/pages/landingPage/index.jsx
+++ src/pages/landingPage/index.jsx
@@ -1,20 +1,21 @@
-import { useScroll, useTransform, motion } from "framer-motion";
+
 import { useEffect, useRef } from "react";
 import Hero from "./section/Hero";
 import About from "./section/About";
+import TechStack from "./section/TechStack";
 
 
 
 const LandingPage = () => {
   
   return (
-    <motion.div
+    <div
    
       className=" w-full scroll-smooth text-primary bg-secondary relative"
     >
       <Hero />
       <About />
-    </motion.div>
+    </div>
   );
 };
 
