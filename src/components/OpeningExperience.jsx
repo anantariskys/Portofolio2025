c5822c10e201c31b06c00eeb70fa74089d46991b
anantariskys <gamersindo555@gmail.com>
2024-12-23 00:43:55 +0700

    Create OpeningExperience.jsx

---
+++ src/components/OpeningExperience.jsx
@@ -0,0 +1,48 @@
+import gsap, { ScrollTrigger } from "gsap/all";
+import React, { useEffect } from "react";
+import { use } from "react";
+
+const OpeningExperience = () => {
+    const titleRef = React.useRef(null);
+    const lineRef = React.useRef(null);
+    const sectionRef = React.useRef(null);
+    gsap.registerPlugin(ScrollTrigger);
+
+    useEffect(() => {
+        gsap.fromTo(titleRef.current,{x:-window.innerWidth} ,{
+            scrollTrigger: {
+                trigger: sectionRef.current,
+                start: "top bottom",
+                end: "bottom bottom",
+                scrub: 1,
+
+            },
+            x: 0,
+            ease: "none",
+        });
+        gsap.fromTo(lineRef.current,{x:window.innerWidth} ,{
+            scrollTrigger: {
+                trigger: sectionRef.current,
+                start: "top bottom",
+                end: "bottom bottom",
+                scrub: 3,
+
+            },
+            x: 0,
+            ease: "none",
+        });
+        
+    })
+  return (
+    <div ref={sectionRef} className="w-screen h-screen overflow-hidden   flex items-center text-secondary">
+      <main className="container ">
+        <div className="flex  justify-start">
+          <h1 ref={titleRef} className="text-7xl font-bold">My Experience</h1>
+        </div>
+      </main>
+      <div ref={lineRef} className="h-4 w-full bg-secondary" />
+    </div>
+  );
+};
+
+export default OpeningExperience;
