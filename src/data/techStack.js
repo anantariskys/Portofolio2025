3567d82ec289681605bcda6bb7d92d442b9beef2
anantariskys <gamersindo555@gmail.com>
2024-12-22 07:56:59 +0700

    Create techStack.js

---
+++ src/data/techStack.js
@@ -0,0 +1,62 @@
+import image1 from "../assets/skill/pngwing.com.png";
+import image2 from "../assets/skill/pngwing.com (1).png";
+import image3 from "../assets/skill/pngwing.com (2).png";
+import image4 from "../assets/skill/pngwing.com (3).png";
+import image5 from "../assets/skill/pngwing.com (4).png";
+import image6 from "../assets/skill/pngwing.com (5).png";
+import image7 from "../assets/skill/pngwing.com (6).png";
+import image8 from "../assets/skill/pngwing.com (7).png";
+import image9 from "../assets/skill/pngwing.com (8).png";
+import image10 from "../assets/skill/pngwing.com (9).png";
+import image11 from "../assets/skill/pngwing.com (10).png";
+import image12 from "../assets/skill/pngwing.com (11).png";
+import image13 from "../assets/skill/pngwing.com (12).png";
+import image14 from "../assets/skill/pngwing.com (13).png";
+import image15 from "../assets/skill/pngwing.com (14).png";
+import image16 from "../assets/skill/pngwing.com (15).png";
+import image17 from "../assets/skill/pngwing.com (16).png";
+import image18 from "../assets/skill/pngwing.com (17).png";
+
+import Laravel from "../assets/skill/Laravel.png";
+import Next from "../assets/skill/Next.js.png";
+import Remix from "../assets/skill/remix-letter-dark.png";
+import Express from "../assets/skill/Express.png";
+import GSAP from "../assets/skill/gsap.png";
+import Firebase from '../assets/skill/firebase.png';
+import Supabase from '../assets/skill/supabase.png';
+import TypeScript from '../assets/skill/Ts.png';
+
+
+export const categorizedTechStack = {
+  languages: [
+    { id: 1, title: "HTML", imageUrl: image1 },
+    { id: 2, title: "CSS", imageUrl: image2 },
+    { id: 3, title: "JavaScript", imageUrl: image3 },
+    { id: 6, title: "TypeScript", imageUrl: TypeScript },
+    { id: 10, title: "PHP", imageUrl: image10 },
+    { id: 13, title: "Java", imageUrl: image13 },
+    { id: 14, title: "Python", imageUrl: image14 },
+  ],
+  frameworksLibraries: [
+    { id: 11, title: "React", imageUrl: image11 },
+    { id: 19, title: "Next.js", imageUrl: Next },
+    { id: 20, title: "Remix", imageUrl: Remix },
+    { id: 21, title: "Express.js", imageUrl: Express },
+    { id: 9, title: "Laravel", imageUrl: Laravel },
+    { id: 22, title: "GSAP", imageUrl: GSAP },
+    { id: 4, title: "SASS", imageUrl: image4 },
+    { id: 5, title: "Bootstrap", imageUrl: image5 },
+    { id: 7, title: "DaisyUI", imageUrl: image7 },
+    { id: 8, title: "Vue", imageUrl: image8 },
+    { id: 12, title: "Vite", imageUrl: image12 },
+    { id: 6, title: "Tailwind CSS", imageUrl: image6 },
+    { id: 15, title: "Framer Motion", imageUrl: image15 },
+    { id: 16, title: "Node.js", imageUrl: image16 },
+  ],
+  databases: [
+    { id: 17, title: "MariaDB", imageUrl: image17 },
+    { id: 18, title: "MySQL", imageUrl: image18 },
+    { id: 23, title: "Firebase", imageUrl: Firebase },
+    { id: 24, title: "Supabase", imageUrl: Supabase },
+  ],
+};
