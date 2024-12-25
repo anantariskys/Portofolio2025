c7a6fbc7d01ce3995bd357dd8f1902ac1d216c7a
anantariskys <gamersindo555@gmail.com>
2024-12-25 09:26:29 +0700

    Update experience.js

---
+++ src/data/experience.js
@@ -0,0 +1,105 @@
+export const experiences = [
+    [
+      {
+        title: "Student Employee as a Web Developer",
+        company: "University of Brawijaya",
+        date: "October 2024 - Present",
+        description:
+          "Developed and maintained web and also managing content management systems. Provided web development solutions for the university's needs.",
+        location: "Malang, East Java, Indonesia",
+      },
+      {
+        title: "Front End Web Developer",
+        company: "BCC FILKOM UB",
+        date: "March 2024 - Present",
+        description:
+          "Provided web application development services utilizing Laravel, React, Remix, Tailwind CSS, and Inertia. Collaborated to create interactive and responsive user interfaces. Optimized performance and user experience.",
+        location: "Malang, East Java, Indonesia",
+      },
+    ],[
+        {
+        title: "Front End Web Developer Intern",
+        company: "BCC FILKOM UB",
+        date: "Feb 2024 - March 2024",
+        description:
+          "Developed a responsive e-commerce web app for preloved goods and ride-sharing services using React and Tailwind CSS. Implemented CRUD operations with Axios and optimized the user experience for mobile and desktop.",
+      },
+      {
+        title: "Web Programming Practicum Assistant",
+        company: "Faculty of Computer Science UB",
+        date: "August 2024 - December 2024",
+        description:
+          "Taught students basic programming concepts and guided them in applying knowledge through assignments and projects.",
+        location: "Malang, East Java, Indonesia",
+      },
+    ],[
+        {
+            title: "Web Application Development Practicum Assistant",
+            company: "Faculty of Computer Science UB",
+            date: "August 2024 - December 2024",
+            description:
+              "Taught students basic programming concepts and guided them in applying knowledge through assignments and projects.",
+            location: "Malang, East Java, Indonesia",
+          },
+          {
+            title: "Vice Head of IT Publication Department",
+            company: "KBMDSI FILKOM UB",
+            date: "January 2024 - December 2024",
+            description:
+              "Supervised IT department performance, developed and maintained the department's website, and mentored staff.",
+            location: "Malang, East Java, Indonesia",
+          },
+    ],
+    [
+      {
+        title: "Staff of Publication and Information Technology",
+        company: "RAJA Brawijaya",
+        date: "June 2024 - August 2024",
+        description:
+          "Developed interactive and responsive websites for over 1,600 new students. Implemented live chat features and supported student task submissions.",
+        location: "Malang, East Java, Indonesia",
+      },
+      {
+        title: "Fullstack Developer",
+        company: "Kampung Budaya UB",
+        date: "July 2024 - September 2024",
+        description:
+          "Translated Figma designs into a responsive website. Designed efficient databases and integrated event registration features.",
+        location: "Malang, East Java, Indonesia",
+      },
+      {
+        title: "Basic Programming Practicum Assistant",
+        company: "Faculty of Computer Science (FILKOM) University of Brawijaya",
+        date: "August 2023 - December 2023",
+        description:
+          "Taught 37 students Java programming. Facilitated practical sessions and provided assessments.",
+        location: "Malang, East Java, Indonesia",
+      },
+      {
+        title: "Frontend Web Developer",
+        company: "Eksekutif Mahasiswa Universitas Brawijaya",
+        date: "January 2024 - December 2024",
+        description:
+          "Oversaw website development and maintenance, ensuring timely updates and functionality.",
+        location: "Malang, East Java, Indonesia",
+      },
+    ],
+    [
+      {
+        title: "Frontend Web Developer",
+        company: "KBMPTI FILKOM UB",
+        date: "September 2022 - December 2022",
+        description:
+          "Learned and implemented web development basics as an intern. Collaborated with mentors to enhance organizational and technical skills.",
+        location: "Malang, East Java, Indonesia",
+      },
+      {
+        title: "Staff of Music Division",
+        company: "Unit Aktivitas Band Universitas Brawijaya",
+        date: "January 2023 - December 2023",
+        description:
+          "Managed indie band programs and participated in band performances.",
+        location: "Malang, East Java, Indonesia",
+      },
+    ],
+  ];
