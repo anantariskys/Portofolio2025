c989517ebe4c05a922c731c01e5355493acdda94
anantariskys <gamersindo555@gmail.com>
2025-01-16 07:21:49 +0700

    feat : add new data

--- src/data/project.js
+++ src/data/project.js
@@ -1,73 +1,101 @@
   export const projects = [
     {
+      "name": "ReelScout",
+      "description": "reelscout is a web application that showcases a variety of movies by fetching data from the TMDB API. This project is built using modern web technologies, offering an intuitive and user-friendly interface for browsing and exploring movies.",
+      "tech_stack": ["React Js", "Tailwind CSS","Axios"],
+      "link": "https://reelscout.vercel.app/",
+      "img_url": "https://i.ibb.co.com/7gd6KRR/image-1.png"
+    },
+    {
+      "name": "VehiTrack",
+      "description": "VehicTrack is an application designed to streamline the management and monitoring of vehicles used in the operations of nickel mining companies. It provides employees with a convenient way to request vehicles, which are then approved by their supervisors through a hierarchical approval process. Additionally, the application features a dashboard for tracking vehicle usage and generates booking reports in Excel format.",
+      "tech_stack": ["React Js", "Tailwind CSS", "Laravel","Inertia Js","Zustand"],
+      "link": "",
+      "img_url": "https://i.ibb.co.com/tqrGCzB/image-3.png"
+    },
+    {
+      "name": "TriviaTales",
+      "description": "Trivia Tales is an interactive trivia quiz application that offers a wide range of engaging questions from various categories. The questions are dynamically fetched from the Open Trivia Database (OpenTDB) API, ensuring users receive a fresh and unique experience every time they play. This application aims to provide a fun and educational way for users to test their knowledge across different topics.",
+      "tech_stack": ["React Js", "Tailwind CSS", "Axios",'Zustand'],
+      "link": "https://triviatales.vercel.app/",
+      "img_url": "https://i.ibb.co.com/XbTtsbF/image-2.png"
+    },
+    {
       "name": "Gen-Free",
-      "description": "A digital platform that facilitates political discussions and aspirations across various groups.",
+      "description": "A digital platform that facilitates political discussions and aspirations across variousgroups, especially Gen-Z, aiming to enhance political participation among young people who are critical, practical, and strategic.",
       "tech_stack": ["Remix Js", "Tailwind CSS", "Supabase"],
       "link": "https://gen-free.vercel.app/",
-      "img_url": ""
+      "img_url": "https://i.ibb.co.com/BnmcTpf/Screenshot-2024-12-20-113339.png"
     },
     {
       "name": "KBMDSI 2024",
-      "description": "A responsive and interactive website for the KBMDSI organization.",
+      "description": "A responsive and interactive website representing the KBMDSI organization for the 2024 period. The website showcases detailed information about the organization itself, serving as a hub for academic resources and articles. Designed to be user- friendly and engaging, it attracts numerous students from the department and proudly holds the position as the second-highest trafficked website within the faculty.",
       "tech_stack": ["Laravel", "Inertia Js", "React Js", "Tailwind CSS", "Framer Motion"],
       "link": "https://kbmdsi.ub.ac.id/",
-      "img_url": ""
+      "img_url": "https://i.ibb.co.com/rH9rjXb/Screenshot-2024-12-20-113918.png"
     },
     {
       "name": "EM UB 2024",
-      "description": "A responsive, elegant, and interactive website for the EM UB organization.",
+      "description": "A responsive, elegant, and interactive website representing the EM UB organizationfor the 2024 period. Serving as a branding platform, the website provides comprehensive information about the organization and features an information portal, reflecting the professionalism and values of EM UB.",
       "tech_stack": ["React Js", "Tailwind CSS", "Framer Motion", "Axios"],
       "link": "https://em.ub.ac.id/",
-      "img_url": ""
+      "img_url": "https://i.ibb.co.com/K2Sx6nf/Screenshot-2024-12-20-123133.png"
     },
     {
       "name": "Kampung Budaya 2024",
-      "description": "A dedicated website for Kampung Budaya, the annual event by EM UB.",
+      "description": "A dedicated website for Kampung Budaya, the annual event organized by EM UB. Kampung Budaya serves as the largest stage for regional forums (Forda) and cultural communities to showcase their talents and achieve recognition. This website functions as a platform for event registration and branding, providing comprehensive information about the event, including schedules, highlights, and participant details.",
       "tech_stack": ["Laravel", "Inertia Js", "React Js", "Tailwind CSS", "Framer Motion"],
       "link": "https://kampungbudaya.ub.ac.id/",
-      "img_url": ""
+      "img_url": "https://i.ibb.co.com/LRqP7GL/Screenshot-2024-12-20-123920.png"
     },
     {
       "name": "Raja Brawijaya 2024",
-      "description": "A website for new students of Universitas Brawijaya to learn about campus life.",
+      "description": "A responsive and interactive website for Raja Brawijaya, the annual event aimed at facilitating new students of Universitas Brawijaya to learn about campus life. Raja Brawijaya comprises two main series: PKKMB (Campus Life Introduction Program) and OH (Organization Expo). This website provides comprehensive information to introduce new students to the university while integrating Raja Apps, a feature designed for assignments and activities specifically for new students.",
       "tech_stack": ["Laravel", "Inertia Js", "React Js", "Tailwind CSS", "Framer Motion"],
       "link": "https://rajabrawijaya.ub.ac.id/",
-      "img_url": ""
+      "img_url": "https://i.ibb.co.com/8r7DWhh/Screenshot-2024-12-20-124347.png"
     },
     {
       "name": "TweetNest",
-      "description": "A platform for facilitating food orders with shopping cart functionality.",
+      "description": "A social media platform inspired by X, offering a sleek and elegant user interface designed to enhance the user experience.The platform includes robust features such as secure user authentication, the ability to post tweets, like and interact with posts, and engage in discussions through a commenting system. Built to encourage meaningful interactions and foster an active user community.",
       "tech_stack": ["Remix Js", "Tailwind CSS", "Prisma", "Firebase"],
       "link": "https://github.com/anantariskys/twitterClone",
-      "img_url": ""
+      "img_url": "https://i.ibb.co.com/pPnGbgc/Screenshot-2024-12-20-130130.png"
     },
     {
       "name": "Balance Box",
-      "description": "A food ordering platform developed as a final project for IT Project Management.",
+      "description": "A platform that offers food ordering features complete with a shopping cart functionality. The platform provides detailed information for each food item, including its nutritional content, ensuring transparency and user convenience. Developed as a final project for the Information Technology Project Management course, this platform demonstrates practical application of IT project management principles in a real-world scenario.",
       "tech_stack": ["Remix JS", "Tailwind CSS", "Supabase"],
       "link": "",
-      "img_url": ""
+      "img_url": "https://i.ibb.co.com/CKL91LQ/Screenshot-2024-12-20-130539.png"
     },
     {
       "name": "JalanKodeku",
-      "description": "A self-paced computer science learning platform in Indonesian.",
+      "description": "JalanKodeku is a self-paced computer science learning platform in Indonesian, designed to assist learners stuck in 'tutorial hell,' individuals who wish to study without enrolling in universities or vocational computer schools, and those with limited English proficiency. The platform offers structured learning roadmaps and curated blog recommendations for effective study. This project serves as the final assignment for the User Interface Design course, showcasing practical implementation of user-centric design principles.",
       "tech_stack": ["React Js", "Tailwind CSS"],
       "link": "https://jalankodeku.vercel.app/",
-      "img_url": ""
+      "img_url": "https://i.ibb.co.com/D7zkDjd/Screenshot-2024-12-20-130806.png"
     },
     {
       "name": "BRAW!",
-      "description": "A platform for buying and selling preloved items.",
+      "description": "A platform developed as part of an internship project, designed to facilitate thebuying and selling of preloved items. This platform not only connects buyers andsellers for secondhand goods but also offers additional services to enhance userconvenience. These include consignment services where users can entrust their itemsto be sold on their behalf and a reliable pickup and delivery feature, making theprocess seamless and hassle-free. With its user-friendly design and comprehensivefeatures, the platform aims to provide a practical and efficient solution for sustainableand responsible shopping.",
       "tech_stack": ["React Js", "Tailwind CSS", "Axios"],
       "link": "https://braw-mager.vercel.app/",
-      "img_url": ""
+      "img_url": "https://i.ibb.co.com/mv1x749/Screenshot-2024-12-20-130929.png"
     },
     {
       "name": "Islamind CMS",
-      "description": "A CMS platform for the mobile application Islamind.",
+      "description": "A CMS platform for the mobile application Islamind, equipped with features for content management. The platform enables CRUD operations for verses and books, the ability to publish and unpublish books, and facilitates participation in discussions by answering user-submitted questions. This system ensures seamless management and interaction to support the application's functionality.",
       "tech_stack": ["React Js", "Tailwind CSS", "Axios"],
       "link": "https://islamind-cms.vercel.app/",
-      "img_url": ""
-    }
+      "img_url": "https://i.ibb.co.com/vLsB6Rk/Screenshot-2024-12-20-132046.png"
+    },
+    {
+      "name": "KBMDSI 2023",
+      "description": "A responsive and interactive website representing the KBMDSI organization for the 2023 period. The website showcases detailed information about the organization itself, serving as a hub for academic resources and articles.",
+      "tech_stack": ["React Js", "Tailwind CSS", "Axios"],
+      "link": "https://kbmdsi.ub.ac.id/",
+      "img_url": "https://i.ibb.co.com/WVwJr8f/Screenshot-2024-12-20-131211.png"
+    },
   ];
 
